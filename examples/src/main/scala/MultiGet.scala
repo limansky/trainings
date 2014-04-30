720c7f34909df5d4eff5cd893f7598e80a7bf2a2
Mike Limansky <mike.limansky@gmail.com>
2014-04-30 13:59:31 +0400

    Refactor MultiGet.

--- examples/src/main/scala/MultiGet.scala
+++ examples/src/main/scala/MultiGet.scala
@@ -4,13 +4,13 @@ import scala.util.{Success, Failure}
 
 object MultiGet extends App {
 
-  val parts = 5
+  val partsNumber = 5
 
   def getSinglePart(link: String): Future[Array[Byte]] = {
     Http(url(link) OK as.Bytes)
   }
 
-  def getMultipart(link: String) = {
+  def getMultipart(link: String): Future[Array[Byte]] = {
 
     def getPart(from: Int, to: Option[Int]): Future[Array[Byte]] = {
       val toStr = to.map(_.toString).getOrElse("")
@@ -25,25 +25,29 @@ object MultiGet extends App {
     } yield h.getFirstValue("Content-Length").toInt
 
     f flatMap { size =>
-      val partSize = size / parts
-      val tasks = (0 to parts - 1).map(i => getPart(i * partSize, Some((i + 1) * partSize - 1))) :+
-                  getPart((parts - 1) * partSize, None)
+      val partSize = size / partsNumber
+      val tasks = (0 to partsNumber - 1).map(i => getPart(i * partSize, Some((i + 1) * partSize - 1))) :+
+                  getPart((partsNumber - 1) * partSize, None)
       Future.sequence(tasks)
-    } fallbackTo {
-      getSinglePart(link)
-    }
-
-    f onComplete {
-      case Success(data) =>
-        val filename = link.substring(link.lastIndexOf('/') + 1)
-        val fs = new FileOutputStream(filename)
-        fs.write(data)
-        fs.close
-        println("Done")
-      case Failure(t) =>
-        println(s"Failed to fetch file from $link, because of ${t.getMessage}")
+    } map {
+      _.toArray.flatten
     }
   }
 
-  args.headOption.foreach(getMultipart)
+  args.toList match {
+    case link :: Nil =>
+      getMultipart(link) fallbackTo {
+        getSinglePart(link)
+      } onComplete {
+        case Success(data) =>
+          val filename = link.substring(link.lastIndexOf('/') + 1)
+          val fs = new FileOutputStream(filename)
+          fs.write(data)
+          fs.close
+          println("Done")
+        case Failure(t) =>
+          println(s"Failed to fetch file from $link, because of ${t.getMessage}")
+      }
+    case _ => println("Usage: multiget <link>")
+  }
 }
