56d55bfb40e98c51446d5da48ccea5d2c32e65ef
Mike Limansky <mike.limansky@gmail.com>
2014-06-02 19:35:56 +0400

    Add no tree calculator.

---
+++ examples/3_morescala/src/main/scala/CalcNoTree.scala
@@ -0,0 +1,19 @@
+import scala.util.parsing.combinator.JavaTokenParsers
+
+trait SimpleCalc extends JavaTokenParsers {
+  def num: Parser[Double] = floatingPointNumber ^^ (x => x.toDouble)
+  def plus: Parser[Double => Double] = "+" ~> term ^^ (x => _ + x)
+  def minus: Parser[Double => Double] = "-" ~> term ^^ (x => _ - x)
+  def times: Parser[Double => Double] = "*" ~> factor ^^ (x => _ * x)
+  def div: Parser[Double => Double] = "/" ~> factor ^^ (x => _ / x)
+  def sum: Parser[Double] = term ~ rep(plus | minus) ^^ { case a ~ b => b.foldLeft(a)((i, f) => f(i)) }
+  def term: Parser[Double] = factor ~ rep(times | div) ^^ { case a ~ b => b.foldLeft(a)((i, f) => f(i)) }
+  def factor = num | "(" ~> sum <~ ")"
+}
+
+object CalcNoTree extends App with SimpleCalc {
+  parseAll(sum, args.mkString) match {
+    case Success(e, _) => println("Result: " + e)
+    case fail: NoSuccess    => println("Parsing failed: " + fail.msg)
+  }
+}
