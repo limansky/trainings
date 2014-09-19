d559c9548168a137b856cb94ea18ed7f9a6024bb
Mike Limansky <mike.limansky@gmail.com>
2014-09-19 18:05:38 +0400

    Add slides about collections and reflection API.

--- examples/3_morescala/src/main/scala/Reflect.scala
+++ examples/3_morescala/src/main/scala/Reflect.scala
@@ -1,9 +1,16 @@
+import scala.reflect.runtime.universe._
+
 object Reflect extends App {
-  def foo(item: Option[Any]) = item match {
-    case t: Option[Int] => println("int")
-    case t: Option[Boolean] => println("bool")
-    case None         => println("none")
+  def foo[T: TypeTag](item: Option[T]) = {
+    val tt = typeOf[T]
+    item match {
+      case t if tt =:= typeOf[Boolean] => println("a bool ")
+      case t if tt =:= typeOf[Int]     => println("an int ")
+      case t if tt <:< typeOf[Seq[_]] => println(s"a sequence ($tt)")
+    }
   }
 
+  foo(Some(5))
   foo(Some(false))
+  foo(Some(List(3,4)))
 }
